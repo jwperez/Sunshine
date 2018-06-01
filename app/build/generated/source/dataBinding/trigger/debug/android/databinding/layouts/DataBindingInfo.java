eddd44346d13ee18865e7cf47509090604336d60
Jonathan Perez <jonathan.perez@me.com>
2018-05-31 20:25:56 -0400

    initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="b8eaaab9-3b56-485c-920b-fbb6a91e7a62")
+public class DataBindingInfo {}
